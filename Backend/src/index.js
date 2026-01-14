64c4ea01fd9392ccb9b5c41f247dcee3be087f4c
Shubham Saini <sshubham.ssaini229@gmail.com>
2026-01-15 04:04:10 +0530

    second commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -28,7 +28,7 @@ app.use(rateLimiter);
 app.use(async (req, res, next) => {
     try {
         // Only connect to Redis if it is NOT already connected
-        if (!redisClient.isOpen) {
+        if (!redisClient.isOpen || !redisClient.isReady) {
             await redisClient.connect();
         }
         await main();
